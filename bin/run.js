2adfb4345c878048b31ca9789729dd4ece6ca301
nakaji-dayo <nakaji.dayo@gmail.com>
2016-03-25 16:30:18 +0900

    fix missing args

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
+try {
 var args = require('schema-args')({
   filename: {
     type: String,
     required: true
   }
 });
+} catch(_) {
+  process.exit(1);
+}
 
 require('../index.js')(args.filename);
 
